fb548dc7948d928ec5d33a31280caa10d157e48a
Nick Hagedorn <nicholas.d.hagedorn@gmail.com>
2022-12-21 22:25:42 -0500

    Added security

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import markdown
 import uuid 
 import hashlib
 import glob
+import string
 from datetime import datetime
 from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
@@ -14,7 +15,7 @@ app = Flask(__name__)
 max_testcases = 10
 games = []
 users = []
-adminPass = []
+adminPass = result_str = ''.join(random.choice(string.ascii_lowercase) for i in range(40))
 
 # PROBLEM CREATION/EDITING/SOLUTION GRADING
 
@@ -57,11 +58,8 @@ def get_problem_names():
     return problems
 
 def admin_check(req):
-    for filename in glob.glob(os.path.join("users/", '*.json')): #only process .JSON files in folder.      
-        with open(filename, encoding='utf-8') as currentFile:
-            data = json.load(currentFile)
-            if req.cookies.get("userid") == data[3] and data[4]:
-                return data
+    if req.cookies.get("userid") == adminPass:
+        return True
     return False
 
 @app.route("/list", methods=["GET","POST"])
@@ -327,7 +325,7 @@ def log_in():
                     data = json.load(currentFile)
                     if is_account(data, emailUsername, password):
 
-                        userID = data[3]
+                        userID = adminPass
                         isAdmin = data[4]
                         
                         return render_template(
